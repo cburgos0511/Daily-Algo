82bc6a4f69c6c358fa09afcd283e1daa08c78929
cburgos0511 <48486237+cburgos0511@users.noreply.github.com>
2020-05-02 23:47:38 -0500

    sum of intergers

---
+++ sumOfint.js
@@ -0,0 +1,8 @@
+function getSumOfDigits(integer) {
+   let sum = 0;
+   let digits = String(integer);
+   for (let i = 0; i < digits.length; i++) {
+      sum += Number(digits[i]);
+   }
+   return sum;
+}
