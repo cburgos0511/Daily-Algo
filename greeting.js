d88bd9da2aa1e686cfc823d32947fa09c7018627
cburgos0511 <48486237+cburgos0511@users.noreply.github.com>
2020-05-20 20:30:52 -0500

    7kyu from codewars

---
+++ greeting.js
@@ -0,0 +1,13 @@
+const sayHello = (name) => {
+   let n = "";
+   for (let i = 0; i < name.length; i++) {
+      if (i === 0) {
+         n += name[i].toUpperCase();
+      } else {
+         n += name[i].toLowerCase();
+      }
+   }
+   return `Hello ${n}!`;
+};
+
+console.log(sayHello("cRuZ"));
